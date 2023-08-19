9fef3ca4d35cceb14ee1f2d72a92d0a64667b2bf
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-08-19 15:15:12 +0200

    Moved log and config initialization to separate func.

---
+++ cmd/indexer/init.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/dipdup-net/go-lib/config"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func initLogConfig() (*Config, error) {
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: "2023-08-17 12:03:04",
+	})
+
+	configPath := rootCmd.PersistentFlags().StringP("config", "c", "dipdup.yml", "path to YAML config file")
+	if err := rootCmd.Execute(); err != nil {
+		log.Panic().Err(err).Msg("command line execute")
+		return nil, err
+	}
+
+	if err := rootCmd.MarkFlagRequired("config"); err != nil {
+		log.Panic().Err(err).Msg("config command line arg is required")
+		return nil, err
+	}
+
+	var cfg Config
+	if err := config.Parse(*configPath, &cfg); err != nil {
+		log.Panic().Err(err).Msg("parsing config file")
+		return nil, err
+	}
+
+	if cfg.LogLevel == "" {
+		cfg.LogLevel = zerolog.LevelInfoValue
+	}
+
+	logLevel, err := zerolog.ParseLevel(cfg.LogLevel)
+	if err != nil {
+		log.Panic().Err(err).Msg("parsing log level")
+		return nil, err
+	}
+	zerolog.SetGlobalLevel(logLevel)
+	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+		return file + ":" + strconv.Itoa(line)
+	}
+	log.Logger = log.Logger.With().Caller().Logger()
+
+	return &cfg, nil
+}
--- cmd/indexer/main.go
+++ cmd/indexer/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"context"
-	"github.com/dipdup-net/go-lib/config"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
@@ -19,48 +16,11 @@ var rootCmd = &cobra.Command{
 }
 
 func main() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: "2023-08-17 12:03:04",
-	})
-	configPath := rootCmd.PersistentFlags().StringP("config", "c", "dipdup.yml", "path to YAML config file")
-	if err := rootCmd.Execute(); err != nil {
-		log.Panic().Err(err).Msg("command line execute")
-		return
-	}
-	if err := rootCmd.MarkFlagRequired("config"); err != nil {
-		log.Panic().Err(err).Msg("config command line arg is required")
-		return
-	}
+	_, err := initLogConfig()
 
-	var cfg Config
-	if err := config.Parse(*configPath, &cfg); err != nil {
-		log.Panic().Err(err).Msg("parsing config file")
-		return
-	}
-
-	if cfg.LogLevel == "" {
-		cfg.LogLevel = zerolog.LevelInfoValue
-	}
-
-	logLevel, err := zerolog.ParseLevel(cfg.LogLevel)
 	if err != nil {
-		log.Panic().Err(err).Msg("parsing log level")
 		return
 	}
-	zerolog.SetGlobalLevel(logLevel)
-	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
-		short := file
-		for i := len(file) - 1; i > 0; i-- {
-			if file[i] == '/' {
-				short = file[i+1:]
-				break
-			}
-		}
-		file = short
-		return file + ":" + strconv.Itoa(line)
-	}
-	log.Logger = log.Logger.With().Caller().Logger()
 
 	ctx, cancel := context.WithCancel(context.Background())
 
