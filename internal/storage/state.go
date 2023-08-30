e02d6a54a505ee7a691ef7428fd5f538179e8720
Alexander <LavyshAlexander@gmail.com>
2023-08-30 14:26:35 +0300

    fix gosum gitattributes (#10)

--- internal/storage/state.go
+++ internal/storage/state.go
@@ -18,10 +18,10 @@ type IState interface {
 
 // State -
 type State struct {
-	bun.BaseModel      `bun:"state" comment:"Current indexer state"`
+	bun.BaseModel      `bun:"state"                     comment:"Current indexer state"`
 	ID                 uint64          `bun:",pk,autoincrement"         comment:"Unique internal identity"`
 	Name               string          `bun:",unique:state_name"        comment:"Indexer name"`
-	LastHeight         Level           `bun:"last_height"          comment:"Last block height"`
+	LastHeight         Level           `bun:"last_height"               comment:"Last block height"`
 	LastTime           time.Time       `bun:"last_time"                 comment:"Time of last block"`
 	ChainId            string          `bun:"chain_id"                  comment:"Celestia chain id"`
 	TotalTx            uint64          `bun:"total_tx"                  comment:"Transactions count in celestia"`
