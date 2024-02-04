d92677ce4fa7cfc4c32eff2fc5f67670a3de462d
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2024-02-04 15:03:28 +0100

    Fix: rollup series (#120)
    * Fix: rollup series
    
    * Fix: group by all providers

--- internal/storage/postgres/storage_test.go
+++ internal/storage/postgres/storage_test.go
@@ -922,54 +922,6 @@ func (s *StorageTestSuite) TestDenomMetadata() {
 	s.Require().Greater(len(m.Units), 0)
 }
 
-func (s *StorageTestSuite) TestRollupLeaderboard() {
-	for _, column := range []string{
-		sizeColumn, blobsCountColumn, timeColumn, "",
-	} {
-		ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer ctxCancel()
-
-		rollups, err := s.storage.Rollup.Leaderboard(ctx, column, sdk.SortOrderDesc, 10, 0)
-		s.Require().NoError(err, column)
-		s.Require().Len(rollups, 3, column)
-
-		rollup := rollups[0]
-		s.Require().EqualValues("Rollup 3", rollup.Name, column)
-		s.Require().EqualValues("The third", rollup.Description, column)
-		s.Require().EqualValues(34, rollup.Size, column)
-		s.Require().EqualValues(3, rollup.BlobsCount, column)
-	}
-}
-
-func (s *StorageTestSuite) TestRollupStats() {
-	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer ctxCancel()
-
-	rollup, err := s.storage.Rollup.Stats(ctx, 1)
-	s.Require().NoError(err)
-
-	s.Require().EqualValues(30, rollup.Size)
-	s.Require().EqualValues(2, rollup.BlobsCount)
-}
-
-func (s *StorageTestSuite) TestRollupNamespaces() {
-	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer ctxCancel()
-
-	nsIds, err := s.storage.Rollup.Namespaces(ctx, 1, 10, 0)
-	s.Require().NoError(err)
-	s.Require().Len(nsIds, 2)
-}
-
-func (s *StorageTestSuite) TestRollupProviders() {
-	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer ctxCancel()
-
-	providers, err := s.storage.Rollup.Providers(ctx, 1)
-	s.Require().NoError(err)
-	s.Require().Len(providers, 2)
-}
-
 func (s *StorageTestSuite) TestNotify() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
