823d681fef0c66de8b2da68d82bdedaa85e55366
Artem <aopoltorzhicky@gmail.com>
2024-02-07 23:06:01 +0100

    Optimization: addresses list query

--- internal/storage/postgres/address.go
+++ internal/storage/postgres/address.go
@@ -5,6 +5,7 @@ package postgres
 
 import (
 	"context"
+
 	"github.com/uptrace/bun"
 
 	"github.com/celenium-io/celestia-indexer/internal/storage"
@@ -34,13 +35,16 @@ func (a *Address) ByHash(ctx context.Context, hash []byte) (address storage.Addr
 }
 
 func (a *Address) ListWithBalance(ctx context.Context, filters storage.AddressListFilter) (result []storage.Address, err error) {
-	query := a.DB().NewSelect().Model(&result).
-		Offset(filters.Offset).
-		Relation("Balance")
+	addressQuery := a.DB().NewSelect().Model((*storage.Address)(nil)).
+		Offset(filters.Offset)
+	addressQuery = addressListFilter(addressQuery, filters)
 
-	query = addressListFilter(query, filters)
-
-	err = query.Scan(ctx)
+	err = a.DB().NewSelect().
+		TableExpr("(?) as address", addressQuery).
+		ColumnExpr("address.*").
+		ColumnExpr("balance.currency as balance__currency, balance.total as balance__total").
+		Join("left join balance on balance.id = address.id").
+		Scan(ctx, &result)
 	return
 }
 
---
+++ internal/storage/postgres/address_test.go
@@ -0,0 +1,93 @@
+// SPDX-FileCopyrightText: 2024 PK Lab AG <contact@pklab.io>
+// SPDX-License-Identifier: MIT
+
+package postgres
+
+import (
+	"context"
+	"time"
+
+	"github.com/celenium-io/celestia-indexer/internal/storage"
+	"github.com/celenium-io/celestia-indexer/internal/storage/types"
+	sdk "github.com/dipdup-net/indexer-sdk/pkg/storage"
+)
+
+func (s *StorageTestSuite) TestAddressByHash() {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer ctxCancel()
+
+	hash := []byte{0xde, 0xce, 0x42, 0x5b, 0x75, 0xd6, 0x71, 0x15, 0xbd, 0xa8, 0x77, 0xe1, 0xe7, 0xa1, 0xf2, 0x62, 0xf6, 0xfa, 0x51, 0xd6}
+	address, err := s.storage.Address.ByHash(ctx, hash)
+	s.Require().NoError(err)
+	s.Require().EqualValues(1, address.Id)
+	s.Require().EqualValues(100, address.Height)
+	s.Require().Equal("celestia1mm8yykm46ec3t0dgwls70g0jvtm055wk9ayal8", address.Address)
+}
+
+func (s *StorageTestSuite) TestAddressList() {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer ctxCancel()
+
+	addresses, err := s.storage.Address.ListWithBalance(ctx, storage.AddressListFilter{
+		Limit:  10,
+		Offset: 0,
+		Sort:   sdk.SortOrderAsc,
+	})
+	s.Require().NoError(err)
+	s.Require().Len(addresses, 2)
+
+	s.Require().EqualValues(1, addresses[0].Id)
+	s.Require().EqualValues(100, addresses[0].Height)
+	s.Require().Equal("celestia1mm8yykm46ec3t0dgwls70g0jvtm055wk9ayal8", addresses[0].Address)
+	s.Require().Equal("123", addresses[0].Balance.Total.String())
+	s.Require().Equal("utia", addresses[0].Balance.Currency)
+
+	s.Require().EqualValues(2, addresses[1].Id)
+	s.Require().EqualValues(101, addresses[1].Height)
+	s.Require().Equal("celestia1jc92qdnty48pafummfr8ava2tjtuhfdw774w60", addresses[1].Address)
+	s.Require().Equal("321", addresses[1].Balance.Total.String())
+	s.Require().Equal("utia", addresses[1].Balance.Currency)
+}
+
+func (s *StorageTestSuite) TestAddressMessages() {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer ctxCancel()
+
+	messages, err := s.storage.Message.ByAddress(ctx, 1, storage.AddressMsgsFilter{
+		Limit:  10,
+		Offset: 0,
+		Sort:   sdk.SortOrderAsc,
+	})
+	s.Require().NoError(err)
+	s.Require().Len(messages, 2)
+
+	msg := messages[0].Msg
+	s.Require().EqualValues(1, msg.Id)
+	s.Require().EqualValues(1000, msg.Height)
+	s.Require().EqualValues(0, msg.Position)
+	s.Require().EqualValues(types.MsgAddressTypeFromAddress, messages[0].Type)
+	s.Require().Equal(types.MsgWithdrawDelegatorReward, msg.Type)
+	s.Require().NotNil(messages[0].Tx)
+}
+
+func (s *StorageTestSuite) TestAddressMessagesWithType() {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer ctxCancel()
+
+	messages, err := s.storage.Message.ByAddress(ctx, 1, storage.AddressMsgsFilter{
+		Limit:        10,
+		Offset:       0,
+		Sort:         sdk.SortOrderAsc,
+		MessageTypes: []string{"MsgWithdrawDelegatorReward", "MsgDelegate"},
+	})
+	s.Require().NoError(err)
+	s.Require().Len(messages, 2)
+
+	msg := messages[0].Msg
+	s.Require().EqualValues(1, msg.Id)
+	s.Require().EqualValues(1000, msg.Height)
+	s.Require().EqualValues(0, msg.Position)
+	s.Require().EqualValues(types.MsgAddressTypeFromAddress, messages[0].Type)
+	s.Require().Equal(types.MsgWithdrawDelegatorReward, msg.Type)
+	s.Require().NotNil(messages[0].Tx)
+}
