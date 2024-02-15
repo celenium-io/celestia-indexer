fd185e57910fb6bc35d163548c996d15a4a51762
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2024-02-15 13:14:40 +0100

    Optimization: blob log height index (#137)

--- internal/storage/postgres/index.go
+++ internal/storage/postgres/index.go
@@ -254,6 +254,15 @@ func createIndices(ctx context.Context, conn *database.Bun) error {
 			Exec(ctx); err != nil {
 			return err
 		}
+		if _, err := tx.NewCreateIndex().
+			IfNotExists().
+			Model((*storage.BlobLog)(nil)).
+			Index("blob_log_height_idx").
+			Column("height").
+			Using("BRIN").
+			Exec(ctx); err != nil {
+			return err
+		}
 
 		// Rollup
 		if _, err := tx.NewCreateIndex().
