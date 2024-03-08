ecf1dd33ed4e2bb3cebe9127ceeff2d8d25f39f6
Alexander <LavyshAlexander@gmail.com>
2024-03-08 09:41:20 +0300

    Update response_test.go, added TestNewResponseRecorder

--- cmd/api/cache/response_test.go
+++ cmd/api/cache/response_test.go
@@ -1,1 +1,17 @@
 package cache
+
+import (
+	_ "bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewResponseRecorder(t *testing.T) {
+	w := httptest.NewRecorder()
+	recorder := NewResponseRecorder(w)
+	if recorder.ResponseWriter != w {
+		t.Errorf("ResponseWriter not set correctly")
+	}
+}
+
