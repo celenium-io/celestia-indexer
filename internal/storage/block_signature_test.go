fe00badb568ff01ae8ec7d8461e9c746213ea4f7
gastlypok <48160281+gastlypok@users.noreply.github.com>
2024-03-10 16:13:17 +0300

    Add TestTableName

--- internal/storage/block_signature_test.go
+++ internal/storage/block_signature_test.go
@@ -1,1 +1,11 @@
 package storage
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestTableName(t *testing.T) {
+    blockSignature := BlockSignature{}
+    assert.Equal(t, "block_signature", blockSignature.TableName())
+}
