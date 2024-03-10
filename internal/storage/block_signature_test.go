bdf2c5ffc0283939225ccd31f36361f2fdce7c83
Lavysh Alexander <LavyshAlexander@gmail.com>
2024-03-10 21:08:30 +0300

    fix go formatting

--- internal/storage/block_signature_test.go
+++ internal/storage/block_signature_test.go
@@ -1,11 +1,11 @@
 package storage
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestTableName(t *testing.T) {
-    blockSignature := BlockSignature{}
-    assert.Equal(t, "block_signature", blockSignature.TableName())
+	blockSignature := BlockSignature{}
+	assert.Equal(t, "block_signature", blockSignature.TableName())
 }
