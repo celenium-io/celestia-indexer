e9ac3061a1f70563dba21c87e34d67b8614f8840
A1exCoon <48766599+A1exCoon@users.noreply.github.com>
2024-03-10 23:07:13 +0300

    Update constant_test.go TestTableName added

--- internal/storage/constant_test.go
+++ internal/storage/constant_test.go
@@ -1,1 +1,11 @@
 package storage
+
+import (
+	"github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestTableName(t *testing.T) {
+  constant := Constant{}
+  assert.Equal(t, "constant", constant.TableName())
+}
