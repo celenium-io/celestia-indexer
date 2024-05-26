42422c3230698913dd542b0605630716862a974e
Artem <aopoltorzhicky@gmail.com>
2024-05-26 23:35:43 +0200

    Try to fix cancellation error

--- cmd/api/init.go
+++ cmd/api/init.go
@@ -142,6 +142,16 @@ func cacheSkipper(c echo.Context) bool {
 func initEcho(cfg ApiConfig, db postgres.Storage, env string) *echo.Echo {
 	e := echo.New()
 	e.Validator = handler.NewCelestiaApiValidator()
+
+	timeout := 30 * time.Second
+	if cfg.RequestTimeout > 0 {
+		timeout = time.Duration(cfg.RequestTimeout) * time.Second
+	}
+	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper: websocketSkipper,
+		Timeout: timeout,
+	}))
+
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
 		LogURI:       true,
 		LogStatus:    true,
@@ -199,15 +209,6 @@ func initEcho(cfg ApiConfig, db postgres.Storage, env string) *echo.Echo {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Secure())
 	e.Pre(middleware.RemoveTrailingSlash())
-
-	timeout := 30 * time.Second
-	if cfg.RequestTimeout > 0 {
-		timeout = time.Duration(cfg.RequestTimeout) * time.Second
-	}
-	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
-		Skipper: websocketSkipper,
-		Timeout: timeout,
-	}))
 
 	if cfg.Prometheus {
 		e.Use(echoprometheus.NewMiddlewareWithConfig(echoprometheus.MiddlewareConfig{
