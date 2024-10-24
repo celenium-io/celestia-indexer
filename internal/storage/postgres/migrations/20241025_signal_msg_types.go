861c56773a98dd57fce214a77f679b81a6719c03
Artem <aopoltorzhicky@gmail.com>
2024-10-25 01:46:59 +0300

    Add DB migration

---
+++ internal/storage/postgres/migrations/20241025_signal_msg_types.go
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2024 PK Lab AG <contact@pklab.io>
+// SPDX-License-Identifier: MIT
+
+package migrations
+
+import (
+	"context"
+
+	"github.com/uptrace/bun"
+)
+
+func init() {
+	Migrations.MustRegister(upSignalMsgTypes, downSignalMsgTypes)
+}
+
+func upSignalMsgTypes(ctx context.Context, db *bun.DB) error {
+	if _, err := db.ExecContext(ctx, `ALTER TYPE msg_type ADD VALUE 'MsgSignalVersion' AFTER 'MsgAcknowledgement'`); err != nil {
+		return err
+	}
+	if _, err := db.ExecContext(ctx, `ALTER TYPE msg_type ADD VALUE 'MsgTryUpgrade' AFTER 'MsgSignalVersion'`); err != nil {
+		return err
+	}
+	return nil
+}
+
+func downSignalMsgTypes(_ context.Context, _ *bun.DB) error {
+	return nil
+}
