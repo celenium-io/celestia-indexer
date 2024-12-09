3a5058df7b6d013009c23531c5644c8e7198ead1
Konst <johnysalvesen@gmail.com>
2024-12-09 19:30:02 +0300

    Changed BlobProof.Nodes field type to [][]byte

--- cmd/api/handler/responses/blobProof.go
+++ cmd/api/handler/responses/blobProof.go
@@ -4,29 +4,22 @@
 package responses
 
 import (
-	"encoding/base64"
 	"github.com/celestiaorg/celestia-app/v3/pkg/proof"
 )
 
 type BlobProof struct {
 	Start int32    `example:"0"  format:"integer" json:"start" swaggertype:"integer"`
 	End   int32    `example:"16" format:"integer" json:"end"   swaggertype:"integer"`
-	Nodes []string `json:"nodes"`
+	Nodes [][]byte `json:"nodes"`
 }
 
 func NewProofs(proofs []*proof.NMTProof) []BlobProof {
 	result := make([]BlobProof, len(proofs))
-
 	for i := range proofs {
-		proofNodes := make([]string, len(proofs[i].Nodes))
-		for j, node := range proofs[i].Nodes {
-			proofNodes[j] = base64.StdEncoding.EncodeToString(node)
-		}
-
 		result[i] = BlobProof{
 			Start: proofs[i].Start,
 			End:   proofs[i].End,
-			Nodes: proofNodes,
+			Nodes: proofs[i].Nodes,
 		}
 	}
 	return result
