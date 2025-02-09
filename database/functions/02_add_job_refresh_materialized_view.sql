7d85f6fd1a8b35956aa86625f573cd89e529ee58
Artem <aopoltorzhicky@gmail.com>
2025-02-09 14:03:09 +0300

    Change refresh time

--- database/functions/02_add_job_refresh_materialized_view.sql
+++ database/functions/02_add_job_refresh_materialized_view.sql
@@ -9,12 +9,12 @@ CREATE OR REPLACE PROCEDURE add_job_refresh_materialized_view()
 
         if not exists (select from timescaledb_information.jobs where proc_name = 'refresh_short_materialized_view')
         then
-            PERFORM add_job('refresh_short_materialized_view', '5m', config => NULL);
+            PERFORM add_job('refresh_short_materialized_view', '15m', config => NULL);
         end if;
 
         if not exists (select from timescaledb_information.jobs where proc_name = 'refresh_da_change_materialized_view')
         then
-            PERFORM add_job('refresh_da_change_materialized_view', '5m', config => NULL);
+            PERFORM add_job('refresh_da_change_materialized_view', '1h', config => NULL);
         end if;
     END
     $$;
