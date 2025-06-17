eeca9a86c616d626c60d7820ff06115891d8b5bc
Artem <aopoltorzhicky@gmail.com>
2025-06-18 01:23:27 +0300

    Remove wrong license

--- cmd/private_api/config.go
+++ cmd/private_api/config.go
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: 2024 PK Lab AG <contact@pklab.io>
 // SPDX-License-Identifier: MIT
 
-// SPDX-FileCopyrightText: 2025 PK Lab AG <contact@pklab.io>
-// SPDX-License-Identifier: MIT
-
 package main
 
 import (
