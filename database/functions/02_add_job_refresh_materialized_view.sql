cc9e51ac158f9abf50355bedf321aab207d58648
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2025-06-25 15:11:12 +0200

    Fix: decrease materialized views refresh period (#407)

--- database/functions/02_add_job_refresh_materialized_view.sql
+++ database/functions/02_add_job_refresh_materialized_view.sql
@@ -4,17 +4,17 @@ CREATE OR REPLACE PROCEDURE add_job_refresh_materialized_view()
     BEGIN
         if not exists (select from timescaledb_information.jobs where proc_name = 'refresh_materialized_view')
         then
-            PERFORM add_job('refresh_materialized_view', '1h', config => NULL);
+            PERFORM add_job('refresh_materialized_view', '15m', config => NULL);
         end if;
 
         if not exists (select from timescaledb_information.jobs where proc_name = 'refresh_short_materialized_view')
         then
-            PERFORM add_job('refresh_short_materialized_view', '15m', config => NULL);
+            PERFORM add_job('refresh_short_materialized_view', '5m', config => NULL);
         end if;
 
         if not exists (select from timescaledb_information.jobs where proc_name = 'refresh_da_change_materialized_view')
         then
-            PERFORM add_job('refresh_da_change_materialized_view', '1h', config => NULL);
+            PERFORM add_job('refresh_da_change_materialized_view', '15m', config => NULL);
         end if;
     END
     $$;
