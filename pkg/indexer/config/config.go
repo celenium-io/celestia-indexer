9d5729eae58c4ad06def1171baa2a3a1a2804fbd
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2025-07-09 15:22:50 +0300

    Fix: update license header (#425)

--- pkg/indexer/config/config.go
+++ pkg/indexer/config/config.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 PK Lab AG <celenium@baking-bad.org>
+// SPDX-FileCopyrightText: 2025 Bb Strategy Pte. Ltd. <celenium@baking-bad.org>
 // SPDX-License-Identifier: MIT
 
 package config
