a36abd46f9a400e9adb221ced30d129efc912e72
Artem <aopoltorzhicky@gmail.com>
2025-10-08 18:14:09 +0300

    Fix: gov v1 params changes

--- pkg/indexer/decode/handle/gov.go
+++ pkg/indexer/decode/handle/gov.go
@@ -56,7 +56,7 @@ func MsgSubmitProposalV1(ctx *context.Context, codec codec.Codec, status storage
 		prpsl.Title = "Proposal with messages"
 	}
 
-	changes := make(map[string]any, 0)
+	changes := make([]paramsV1Beta.ParamChange, 0)
 	var sb strings.Builder
 	if _, err := sb.WriteString("Proposal contains messages:\r\n"); err != nil {
 		return msgType, addresses, nil, nil, errors.Wrap(err, "building proposal description from messages")
@@ -69,7 +69,6 @@ func MsgSubmitProposalV1(ctx *context.Context, codec codec.Codec, status storage
 				return msgType, addresses, nil, nil, errors.Wrap(err, "unmarshalling proposal with slashing.v1beta1.MsgUpdateParams")
 			}
 			if p := params.GetParams(); p != nil {
-				paramChanges := make(map[string]any)
 				prpsl.Type = storageTypes.ProposalTypeParamChanged
 
 				var slashFractionDoubleSign math.LegacyDec
@@ -77,28 +76,26 @@ func MsgSubmitProposalV1(ctx *context.Context, codec codec.Codec, status storage
 					return msgType, addresses, nil, nil, errors.Wrap(err, "slash_fraction_double_sign")
 				}
 				ctx.AddConstant(storageTypes.ModuleNameSlashing, "slash_fraction_double_sign", slashFractionDoubleSign.String())
-				paramChanges["slash_fraction_double_sign"] = slashFractionDoubleSign.String()
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameSlashing.String(), "slash_fraction_double_sign", slashFractionDoubleSign.String()))
 
 				var slashFractionDowntime math.LegacyDec
 				if err := slashFractionDoubleSign.Unmarshal(p.GetSlashFractionDowntime()); err != nil {
 					return msgType, addresses, nil, nil, errors.Wrap(err, "slash_fraction_downtime")
 				}
 				ctx.AddConstant(storageTypes.ModuleNameSlashing, "slash_fraction_downtime", slashFractionDowntime.String())
-				paramChanges["slash_fraction_downtime"] = slashFractionDowntime.String()
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameSlashing.String(), "slash_fraction_downtime", slashFractionDowntime.String()))
 
 				downtimeJailDuration := strconv.FormatInt(int64(p.GetDowntimeJailDuration().GetNanos()), 10)
 				ctx.AddConstant(storageTypes.ModuleNameSlashing, "downtime_jail_duration", downtimeJailDuration)
-				paramChanges["downtime_jail_duration"] = downtimeJailDuration
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameSlashing.String(), "downtime_jail_duration", downtimeJailDuration))
 
 				minSignedPerWindow := string(p.GetMinSignedPerWindow())
 				ctx.AddConstant(storageTypes.ModuleNameSlashing, "min_signed_per_window", minSignedPerWindow)
-				paramChanges["min_signed_per_window"] = minSignedPerWindow
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameSlashing.String(), "min_signed_per_window", minSignedPerWindow))
 
 				signedBlocksWindow := strconv.FormatInt(p.GetSignedBlocksWindow(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameSlashing, "signed_blocks_window", signedBlocksWindow)
-				paramChanges["signed_blocks_window"] = signedBlocksWindow
-
-				changes[storageTypes.ModuleNameSlashing.String()] = paramChanges
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameSlashing.String(), "signed_blocks_window", signedBlocksWindow))
 			}
 
 		case "/cosmos.distribution.v1beta1.MsgUpdateParams":
@@ -107,83 +104,75 @@ func MsgSubmitProposalV1(ctx *context.Context, codec codec.Codec, status storage
 				return msgType, addresses, nil, nil, errors.Wrap(err, "unmarshalling proposal with cosmos.distribution.v1beta1.MsgUpdateParams")
 			}
 
-			paramChanges := make(map[string]any)
 			prpsl.Type = storageTypes.ProposalTypeParamChanged
 
 			communityTax := params.Params.CommunityTax.String()
 			ctx.AddConstant(storageTypes.ModuleNameDistribution, "community_tax", communityTax)
-			paramChanges["community_tax"] = communityTax
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameDistribution.String(), "community_tax", communityTax))
 
 			baseProposerReward := params.Params.BaseProposerReward.String() //nolint
 			ctx.AddConstant(storageTypes.ModuleNameDistribution, "base_proposer_reward", baseProposerReward)
-			paramChanges["base_proposer_reward"] = baseProposerReward
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameDistribution.String(), "base_proposer_reward", baseProposerReward))
 
 			bonusProposerReward := params.Params.BonusProposerReward.String() //nolint
 			ctx.AddConstant(storageTypes.ModuleNameDistribution, "bonus_proposer_reward", bonusProposerReward)
-			paramChanges["bonus_proposer_reward"] = bonusProposerReward
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameDistribution.String(), "bonus_proposer_reward", bonusProposerReward))
 
 			withdrawAddrEnabled := strconv.FormatBool(params.Params.WithdrawAddrEnabled)
 			ctx.AddConstant(storageTypes.ModuleNameDistribution, "withdraw_addr_enabled", withdrawAddrEnabled)
-			paramChanges["withdraw_addr_enabled"] = withdrawAddrEnabled
-
-			changes[storageTypes.ModuleNameDistribution.String()] = paramChanges
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameDistribution.String(), "withdraw_addr_enabled", withdrawAddrEnabled))
 
 		case "/cosmos.gov.v1.MsgUpdateParams":
 			var params v1.MsgUpdateParams
 			if err := codec.Unmarshal(msg.Messages[i].Value, &params); err != nil {
 				return msgType, addresses, nil, nil, errors.Wrap(err, "unmarshalling proposal with cosmos.gov.v1.MsgUpdateParams")
 			}
-			paramChanges := make(map[string]any)
+
 			prpsl.Type = storageTypes.ProposalTypeParamChanged
 
 			if minDeposits := params.Params.GetMinDeposit(); len(minDeposits) > 0 {
 				ctx.AddConstant(storageTypes.ModuleNameGov, "min_deposit", minDeposits[0].Amount.String())
-				paramChanges["min_deposit"] = minDeposits[0].Amount.String()
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameGov.String(), "min_deposit", minDeposits[0].Amount.String()))
 			}
 
 			if maxDepositPeriod := params.Params.GetMaxDepositPeriod(); maxDepositPeriod != nil {
 				value := strconv.FormatInt(maxDepositPeriod.Nanoseconds(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameGov, "max_deposit_period", value)
-				paramChanges["max_deposit_period"] = value
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameGov.String(), "max_deposit_period", value))
 			}
 
 			if votingPeriod := params.Params.GetVotingPeriod(); votingPeriod != nil {
 				value := strconv.FormatInt(votingPeriod.Nanoseconds(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameGov, "voting_period", value)
-				paramChanges["voting_period"] = value
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameGov.String(), "voting_period", value))
 			}
 
 			quorum := params.Params.GetQuorum()
 			ctx.AddConstant(storageTypes.ModuleNameGov, "quorum", quorum)
-			paramChanges["quorum"] = quorum
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameGov.String(), "quorum", quorum))
 
 			threshold := params.Params.GetThreshold()
 			ctx.AddConstant(storageTypes.ModuleNameGov, "threshold", threshold)
-			paramChanges["threshold"] = threshold
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameGov.String(), "threshold", threshold))
 
 			vetoThreshold := params.Params.GetVetoThreshold()
 			ctx.AddConstant(storageTypes.ModuleNameGov, "veto_threshold", vetoThreshold)
-			paramChanges["veto_threshold"] = vetoThreshold
-
-			changes[storageTypes.ModuleNameGov.String()] = paramChanges
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameGov.String(), "veto_threshold", vetoThreshold))
 
 		case "/celestia.blob.v1.MsgUpdateBlobParams":
 			var params blobTypes.MsgUpdateBlobParams
 			if err := codec.Unmarshal(msg.Messages[i].Value, &params); err != nil {
 				return msgType, addresses, nil, nil, errors.Wrap(err, "unmarshalling proposal with cosmos.gov.v1.MsgUpdateParams")
 			}
-			paramChanges := make(map[string]any)
 			prpsl.Type = storageTypes.ProposalTypeParamChanged
 
 			gasBlobPerByte := strconv.FormatUint(uint64(params.Params.GetGasPerBlobByte()), 10)
 			ctx.AddConstant(storageTypes.ModuleNameBlob, "gas_per_blob_byte", gasBlobPerByte)
-			paramChanges["gas_per_blob_byte"] = gasBlobPerByte
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameBlob.String(), "gas_per_blob_byte", gasBlobPerByte))
 
 			maxSquareSize := strconv.FormatUint(params.Params.GetGovMaxSquareSize(), 10)
 			ctx.AddConstant(storageTypes.ModuleNameBlob, "gov_max_square_size", maxSquareSize)
-			paramChanges["gov_max_square_size"] = maxSquareSize
-
-			changes[storageTypes.ModuleNameBlob.String()] = paramChanges
+			changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameBlob.String(), "gov_max_square_size", maxSquareSize))
 
 		case "/cosmos.consensus.v1.MsgUpdateParams":
 			var params consensusv1.MsgUpdateParams
@@ -191,36 +180,33 @@ func MsgSubmitProposalV1(ctx *context.Context, codec codec.Codec, status storage
 				return msgType, addresses, nil, nil, errors.Wrap(err, "unmarshalling proposal with cosmos.gov.v1.MsgUpdateParams")
 			}
 
-			paramChanges := make(map[string]any)
 			prpsl.Type = storageTypes.ProposalTypeParamChanged
 
 			if block := params.GetBlock(); block != nil {
 				maxBytes := strconv.FormatInt(block.GetMaxBytes(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameConsensus, "block_max_bytes", maxBytes)
-				paramChanges["block_max_bytes"] = maxBytes
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameConsensus.String(), "block_max_bytes", maxBytes))
 
 				maxGas := strconv.FormatInt(block.GetMaxGas(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameConsensus, "block_max_gas", maxGas)
-				paramChanges["block_max_gas"] = maxGas
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameConsensus.String(), "block_max_gas", maxGas))
 			}
 
 			if evidence := params.GetEvidence(); evidence != nil {
 				maxAgeNumBlocks := strconv.FormatInt(evidence.GetMaxAgeNumBlocks(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameConsensus, "evidence_max_age_num_blocks", maxAgeNumBlocks)
-				paramChanges["evidence_max_age_num_blocks"] = maxAgeNumBlocks
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameConsensus.String(), "evidence_max_age_num_blocks", maxAgeNumBlocks))
 
 				maxBytes := strconv.FormatInt(evidence.GetMaxBytes(), 10)
 				ctx.AddConstant(storageTypes.ModuleNameConsensus, "evidence_max_bytes", maxBytes)
-				paramChanges["evidence_max_bytes"] = maxBytes
+				changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameConsensus.String(), "evidence_max_bytes", maxBytes))
 
 				if age := evidence.GetMaxAgeDuration(); age != nil {
 					value := strconv.FormatInt(int64(age.GetNanos()), 10)
 					ctx.AddConstant(storageTypes.ModuleNameConsensus, "evidence_max_age_duration", value)
-					paramChanges["evidence_max_age_duration"] = value
+					changes = append(changes, paramsV1Beta.NewParamChange(storageTypes.ModuleNameConsensus.String(), "evidence_max_age_duration", value))
 				}
 			}
-
-			changes[storageTypes.ModuleNameConsensus.String()] = paramChanges
 		}
 
 		if _, err := sb.WriteString(fmt.Sprintf("%d. %s\r\n", i+1, msg.Messages[i].TypeUrl)); err != nil {
